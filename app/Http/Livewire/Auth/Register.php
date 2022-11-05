7c3ea7b584b0d0fd0046b2fb706332452436d0cf
QuyHoangMor <quyhx@mor.com.vn>
2022-11-05 17:41:20 +0700

    Hiện Thông báo khi send form hoặc click button

--- app/Http/Livewire/Auth/Register.php
+++ app/Http/Livewire/Auth/Register.php
@@ -8,18 +8,19 @@ use Livewire\Component;
 
 class Register extends Component
 {
-    public $email ;
+    public $saved = false;
+    public $email ='demo@gmail.com';
     public $password;
     public $passwordConfirm;
 
-    public function updated($field)
-    {
-        $this->validateOnly($field, [
-            'email' => 'required|email|unique:users,email',
-            'password' => 'required|min:8',
-            'passwordConfirm' => 'required|same:password',
-        ]);
-    }
+    // public function updated($field)
+    // {
+    //     $this->validateOnly($field, [
+    //         'email' => 'required|email|unique:users,email',
+    //         'password' => 'required|min:8',
+    //         'passwordConfirm' => 'required|same:password',
+    //     ]);
+    // }
 
     public function register()
     {
@@ -27,8 +28,22 @@ class Register extends Component
             'email' => $this->email,
             'password' => Hash::make($this->password),
         ]);
+        $this->saved = true;
+    }
 
-        return redirect()->with('success', 'You have been registered successfully!');
+    public function updating($field){
+         dump($this->email);
+         dump($this->saved);
+         dump($field);
+    }
+
+    public function updated($field){
+        dump($this->email);
+        dump($this->saved);
+        dump($field);
+       if($field !== 'saved'){
+           $this->saved = false;
+       }
     }
 
     public function render()
--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -1,21 +1,30 @@
-<form wire:submit.prevent="register" >
-    <div>
-        <label for="email">Email</label>
-        <input type="email" name="email" id="email" wire:model="email">
-        @error('email') <span class="error">{{ $message }}</span> @enderror
-    </div>
-    <div>
-        <label for="password">Password</label>
-        <input type="password" name="password" id="password" wire:model="password">
-        @error('password') <span class="error">{{ $message }}</span> @enderror
-    </div>
-    <div>
-        <label for="password_confirmation">Confirm Password</label>
-        <input type="password" name="password_confirmation" id="passwordConfirm" wire:model="passwordConfirm">
-        @error('password_confirmation') <span class="error">{{ $message }}</span> @enderror
-    </div>
-    <div>
-        <button type="submit" >Register</button>
-    </div>
-
-</form>
+<div>
+    <form wire:submit.prevent="register" >
+        <div>
+            @if($saved)
+                <div class="alert alert-success">
+                Đăng ký thành công
+                    <button type="button" wire:click="$set('saved',false)">X</button>
+                </div>
+            @endif
+        </div>
+        <div>
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email" wire:model="email">
+            @error('email') <span class="error">{{ $message }}</span> @enderror
+        </div>
+        <div>
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password" wire:model="password">
+            @error('password') <span class="error">{{ $message }}</span> @enderror
+        </div>
+        <div>
+            <label for="password_confirmation">Confirm Password</label>
+            <input type="password" name="password_confirmation" id="passwordConfirm" wire:model="passwordConfirm">
+            @error('password_confirmation') <span class="error">{{ $message }}</span> @enderror
+        </div>
+        <div>
+            <button type="submit" >Register</button>
+        </div>
+    </form>
+</div>
